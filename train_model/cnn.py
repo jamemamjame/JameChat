f442c81bc510a05b0ff8057ed0ac0e7ef6982717
jamemamjame <jamemamjame@gmail.com>
2018-03-08 22:52:19 +0700

    first cnn

--- train_model/cnn.py
+++ train_model/cnn.py
@@ -1,9 +1,16 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib import learn
+from tensorflow.contrib import learn, layers
 from tensorflow.contrib.learn import *
 
 tf.logging.set_verbosity(tf.logging.INFO)
+
+n_sample = 4
+n_filter = 10   # n_filter is used to capture N phrase in any position in sentence
+n_grams = 2     # n_grams is used to group N word be 1 phrase
+max_word = 20
+emb_dim = 100
+n_chanel = 1
 
 
 def cnn_model_fn(features, labels, mode, params):
@@ -16,47 +23,79 @@ def cnn_model_fn(features, labels, mode, params):
     :return:
     '''
     query = features['query']
-    response = features['response']
-
-    # maybe we need to reshape to add n_chanel at the last shape (like a grey-scale image)
-    # query = tf.reshape(query, (query.shape[0], query.shape[1], query.shape[2], 1))
-
-    n_filter = 10  # n_filter is used to capture N phrase in any position in sentence
-    n_grams = 2  # n_grams is used to group N word be 1 phrase
+    pos_response = features['pos_response']
+    neg_response = features['neg_response']
 
     with tf.variable_scope("embedding"):
-        # shape = (maxwords, emb_dim, n_filter=10)
+        # this is filter ot kernel
+        # shape = [max_words, emb_dim, n_filter=10]
         emb_w = tf.get_variable("emb_w", shape=[params['n_maxword'], params['emb_dim'], n_filter],
                                 initializer=tf.random_normal_initializer)
         emb_b = tf.get_variable("emb_b", shape=[n_filter],
                                 initializer=tf.zeros_initializer)
 
-    # Convolution Layer (try to embed query with the same weight on response)
-    conv_query = tf.layers.conv2d(
-        inputs=query,
-        filters=emb_w,
-        kernel_size=[1, params['emb_dim']],
-        padding="same",
-        strides=[0, 1],
-        activation=tf.nn.relu,
-    )
-    # Pooling Layer #1
-    # pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-    pool_query = tf.layers.max_pooling2d(inputs=conv_query, pool_size=[1, params['emb_dim']], strides=[0, 1])
-
-    # pool_flat = tf.reshape(pool_query, [-1, 32 * 32 * 64])
-
-    # Convolution Layer (try to embed response with the same weight on query)
-
-    def conv2d(x, W):
-        return tf.layers.conv2d(
-            inputs=query,
-            filters=emb_w,
-            kernel_size=[1, params['emb_dim']],
-            padding="same",
-            strides=[0, 1],
-            activation=tf.nn.relu,
+    def embed_sentence(x, W, B):
+        convol = tf.nn.conv2d(
+            input=x,
+            filter=W,
+            padding="SAME",
+            strides=[1, 1, 1, 1],
         )
+        convol = tf.nn.relu(convol + B)
+
+        # pooling output size: n_sample x max_word x 1 x n_filter
+        pooling = tf.nn.max_pool(convol, ksize=[1, 1, emb_dim, 1], strides=[1, 1, 1, 1], padding="VALID")
+
+        # Dense layer
+        # reshape [n_samples, max_word, 1, n_filter] ->> [n_samples, max_word * 1 * n_filter]
+        # flatten = tf.reshape(pooling, shape=[-1, max_word * 1 * n_filter])
+        flatten = layers.flatten(pooling)
+
+        # how many dim of vector for represent this sentence (x)
+        embed_dim = 50
+        dense = tf.layers.dense(inputs=flatten, units=embed_dim, activation=tf.nn.relu)
+        return dense
+
+    def cosine_similarity(vec1, vec2):
+        '''
+        calculate cosine_similarity of each sample
+        by reduce_sum(from A•B) / (norm(A) * norm(B))
+        :param vec1: batch of vector1
+        :param vec2: batch of vector2
+        :return:
+        '''
+
+        # calculate (norm(A) * norm(B))
+        ### output.shape = [n_sample, ]
+        vec_norm = tf.norm(vec1, axis=1) * tf.norm(vec2, axis=1)
+
+        # multiply sub_vec vs sub_vec.
+        # output.shape = [n_sample , emb_dim]
+        mul = tf.multiply(vec1, vec2)
+
+        ### sum values in emb_dim for each sample so output.shape = [n_sample, ]
+        reduce_sum = tf.reduce_sum(mul, axis=1)
+
+        # calculate cosine similarity.
+        # output.shape = [n_sample, ]
+        cosine_sim = reduce_sum / vec_norm
+
+        return cosine_sim
+
+    # embedding: get vec.shape = [n_sample , emb_dim]
+    vec1 = embed_sentence(query, emb_w, emb_b)  # query
+    vec2 = embed_sentence(pos_response, emb_w, emb_b)  # pos_response
+    vec3 = embed_sentence(neg_response, emb_w, emb_b)  # neg_response
+
+    # calculate cosine similarity of each vec pairs, output.shape = [n_sample, ]
+    cosine_sim_pos = cosine_similarity(vec1, vec2)  # need a large value
+    cosine_sim_neg = cosine_similarity(vec1, vec3)  # need a tiny value
+
+    # calculate loss of each pair pos_neg. output.shape = [n_sample, ]
+    each_loss = -cosine_sim_pos + cosine_sim_neg  # << too small too good, boundary [-1, 1]
+
+    # sum all loss. get output be scalar
+    total_loss = tf.reduce_sum(each_loss)
 
     # Calculate Loss (for both TRAIN and EVAL modes)
     if mode != learn.ModeKeys.INFER:
--- train_model/prepare_data.py
+++ train_model/prepare_data.py
@@ -13,7 +13,8 @@ dialogs = [
     "Beijing is a historical city that can be traced back to 3,000 years ago.",
     "The city's history dates back three millennia. As the last of the Four Great Ancient Capitals of China",
     "Beijing has been the political center of the country for much of the past eight centuries",
-    "With mountains surrounding the inland city on three sides"
+    "With mountains surrounding the inland city on three sides",
+    "When you want to use a different language, select the same Keyboard & Character Viewer item in the menu bar -- although, when more than one language is enabled, the icon will now show the flag or symbol of the current language in use -- and click on the desired language. The Keyboard Viewer will then change layout to represent the keys of the desired language and allow you to insert native characters. To change back to your regular language, once again click on the Keyboard & Character Viewer menu bar item and select the desired input language to switch to."
 ]
 
 # load pre-trained Word Embedding model
@@ -21,6 +22,7 @@ WORD_EMB = load_word_embedding(load_glove=True)
 
 # constant of maximum number of word in sentence (important when padding)
 MAX_WORD = 20
+N_CHANEL = 1
 
 
 def generate_wordlist_emb(txt):
@@ -56,7 +58,12 @@ def generate_dialogs_embedding(dialogs):
         tmp_dialogs.append(generate_sentence_embedding(sentence))
 
     # padding a word that lower than max_words
-    return np.array(tmp_dialogs)
+    tmp_dialogs = np.array(tmp_dialogs)
+
+    n_sample, max_word, emb_dim, = tmp_dialogs.shape[0], tmp_dialogs.shape[1], tmp_dialogs.shape[2]
+
+    # return with add 1 chanel in the last index
+    return np.reshape(tmp_dialogs, [n_sample, max_word, emb_dim, N_CHANEL])
 
 
 dialogs_emb = generate_dialogs_embedding(dialogs)
