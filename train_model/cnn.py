fdb68c2c5a1f3a1f74d43f35e3003e8a2f9c5f31
jamemamjame <jamemamjame@gmail.com>
2018-03-08 18:35:41 +0700

    complete cal loss without vec3

--- train_model/cnn.py
+++ train_model/cnn.py
@@ -21,14 +21,14 @@ def cnn_model_fn(features, labels, mode, params):
     # maybe we need to reshape to add n_chanel at the last shape (like a grey-scale image)
     # query = tf.reshape(query, (query.shape[0], query.shape[1], query.shape[2], 1))
 
-    n_filter = 10  # n_filter is used to capture N pharse in any position in sentence
+    n_filter = 10  # n_filter is used to capture N phrase in any position in sentence
     n_grams = 2  # n_grams is used to group N word be 1 phrase
 
     with tf.variable_scope("embedding"):
         # shape = (maxwords, emb_dim, n_filter=10)
         emb_w = tf.get_variable("emb_w", shape=[params['n_maxword'], params['emb_dim'], n_filter],
                                 initializer=tf.random_normal_initializer)
-        emb_b = tf.get_variable("emb_b", shape=[params['n_maxword'], params['emb_dim'], n_filter],
+        emb_b = tf.get_variable("emb_b", shape=[n_filter],
                                 initializer=tf.zeros_initializer)
 
     # Convolution Layer (try to embed query with the same weight on response)
@@ -42,17 +42,21 @@ def cnn_model_fn(features, labels, mode, params):
     )
     # Pooling Layer #1
     # pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-    pool_query = tf.layers.max_pooling2d(inputs=conv_query, pool_size=[1, params['emb_dim']])
+    pool_query = tf.layers.max_pooling2d(inputs=conv_query, pool_size=[1, params['emb_dim']], strides=[0, 1])
+
+    # pool_flat = tf.reshape(pool_query, [-1, 32 * 32 * 64])
 
     # Convolution Layer (try to embed response with the same weight on query)
-    conv_response = tf.layers.conv2d(
-        inputs=query,
-        filters=emb_w,
-        kernel_size=[3, 3],
-        padding="same",
-        strides=[0, 1],
-        activation=tf.nn.relu,
-    )
+
+    def conv2d(x, W):
+        return tf.layers.conv2d(
+            inputs=query,
+            filters=emb_w,
+            kernel_size=[1, params['emb_dim']],
+            padding="same",
+            strides=[0, 1],
+            activation=tf.nn.relu,
+        )
 
     # Calculate Loss (for both TRAIN and EVAL modes)
     if mode != learn.ModeKeys.INFER:
